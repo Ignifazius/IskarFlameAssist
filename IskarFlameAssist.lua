8b5ae9907668f243210fe953eb0f4ea412cf8a0a
Ignifazius <ignifazius@trollstar-gaming.de>
2016-01-27 23:12:07 +0100

    adding buttons for FLAME and a list + button for FLAMEALL

--- IskarFlameAssist.lua
+++ IskarFlameAssist.lua
@@ -4,6 +4,10 @@ local carrierName = "NOBODY"
 local lastCarrier = "XX";
 local scanEnabled = false;
 local holdingSince = 0;
+local flameTable = {};
+local hiscoreTable;
+local lastCarrierTime = 0;
+local tableindex = 1;
 
 
 local sBFrame = CreateFrame("Frame","statusBarFrame",UIParent)
@@ -30,20 +34,20 @@ local sBTexture = sBFrame:CreateTexture(nil,"BACKGROUND")
 	sBFrame.text = sbtext;
 
 
-local debugButton = CreateFrame("Button", "Buff", statusBarFrame)
-	debugButton:SetPoint("TOPLEFT", statusBarFrame, 0, 15)
-	debugButton:SetWidth(100)
-	debugButton:SetHeight(25)
-	local text = debugButton:CreateFontString(nil, "OVERLAY")
+local scanButton = CreateFrame("Button", "Buff", statusBarFrame)
+	scanButton:SetPoint("TOPLEFT", statusBarFrame, 0, 15)
+	scanButton:SetWidth(100)
+	scanButton:SetHeight(25)
+	local text = scanButton:CreateFontString(nil, "OVERLAY")
 	text:SetFont("Fonts\\ARIALN.TTF", 11, nil)
 	text:SetPoint("CENTER",-19,5)
 	text:SetText("start scan")
-	debugButton.text = text
-	debugButton:SetNormalFontObject("GameFontNormalSmall")       
-	debugButton:SetNormalTexture("Interface/Buttons/UI-Panel-Button-Up")
-	debugButton:SetHighlightTexture("Interface/Buttons/UI-Panel-Button-Highlight")
-	debugButton:SetPushedTexture("Interface/Buttons/UI-Panel-Button-Down")
-	debugButton:SetScript("OnMouseUp", function(self, button)
+	scanButton.text = text
+	scanButton:SetNormalFontObject("GameFontNormalSmall")       
+	scanButton:SetNormalTexture("Interface/Buttons/UI-Panel-Button-Up")
+	scanButton:SetHighlightTexture("Interface/Buttons/UI-Panel-Button-Highlight")
+	scanButton:SetPushedTexture("Interface/Buttons/UI-Panel-Button-Down")
+	scanButton:SetScript("OnMouseUp", function(self, button)
 		if button == "LeftButton" then
 			scanEnabled = not scanEnabled;
 			if scanEnabled then
@@ -53,9 +57,74 @@ local debugButton = CreateFrame("Button", "Buff", statusBarFrame)
 			end
 			
 		elseif button == "RightButton" then
-			scanEnabled = false;
+			sBFrame:Hide();
 		end
 	end)
+	
+	
+local flameButton = CreateFrame("Button", "Buff", statusBarFrame)
+	flameButton:SetPoint("TOPLEFT", statusBarFrame, 70, 15)
+	flameButton:SetWidth(100)
+	flameButton:SetHeight(25)
+	local text2 = flameButton:CreateFontString(nil, "OVERLAY")
+	text2:SetFont("Fonts\\ARIALN.TTF", 11, nil)
+	text2:SetPoint("CENTER",-19,5)
+	text2:SetText("FLAME")
+	flameButton.text = text2
+	flameButton:SetNormalFontObject("GameFontNormalSmall")       
+	flameButton:SetNormalTexture("Interface/Buttons/UI-Panel-Button-Up")
+	flameButton:SetHighlightTexture("Interface/Buttons/UI-Panel-Button-Highlight")
+	flameButton:SetPushedTexture("Interface/Buttons/UI-Panel-Button-Down")
+	flameButton:SetScript("OnMouseUp", function(self, button)
+		if button == "LeftButton" then
+			flameActual();		
+		elseif button == "RightButton" then
+		end
+	end)
+	
+local flameAllButton = CreateFrame("Button", "Buff", statusBarFrame)
+	flameAllButton:SetPoint("TOPLEFT", statusBarFrame, 140, 15)
+	flameAllButton:SetWidth(100)
+	flameAllButton:SetHeight(25)
+	local text3 = flameAllButton:CreateFontString(nil, "OVERLAY")
+	text3:SetFont("Fonts\\ARIALN.TTF", 11, nil)
+	text3:SetPoint("CENTER",-19,5)
+	text3:SetText("FLAME ALL")
+	flameAllButton.text = text3
+	flameAllButton:SetNormalFontObject("GameFontNormalSmall")       
+	flameAllButton:SetNormalTexture("Interface/Buttons/UI-Panel-Button-Up")
+	flameAllButton:SetHighlightTexture("Interface/Buttons/UI-Panel-Button-Highlight")
+	flameAllButton:SetPushedTexture("Interface/Buttons/UI-Panel-Button-Down")
+	flameAllButton:SetScript("OnMouseUp", function(self, button)
+		if button == "LeftButton" then
+			printFlameList();
+		elseif button == "RightButton" then
+		end
+	end)
+	
+	
+	
+local debugButton = CreateFrame("Button", "Buff", statusBarFrame)
+	debugButton:SetPoint("TOPLEFT", statusBarFrame, 0, 40)
+	debugButton:SetWidth(100)
+	debugButton:SetHeight(25)
+	local text4 = debugButton:CreateFontString(nil, "OVERLAY")
+	text4:SetFont("Fonts\\ARIALN.TTF", 11, nil)
+	text4:SetPoint("CENTER",-19,5)
+	text4:SetText("debug")
+	debugButton.text = text3
+	debugButton:SetNormalFontObject("GameFontNormalSmall")       
+	debugButton:SetNormalTexture("Interface/Buttons/UI-Panel-Button-Up")
+	debugButton:SetHighlightTexture("Interface/Buttons/UI-Panel-Button-Highlight")
+	debugButton:SetPushedTexture("Interface/Buttons/UI-Panel-Button-Down")
+	debugButton:SetScript("OnMouseUp", function(self, button)
+		if button == "LeftButton" then
+			addToList(tableindex, "1.5" , "Test_"..tableindex);		
+			tableindex = tableindex+1;
+		elseif button == "RightButton" then
+		end
+	end)
+	
 	
 	
 function findBuff(target)
@@ -97,17 +166,69 @@ function scanRaidForBuff()
 	end
 end
 
+function addToList(count, time, name)
+	print("adding "..name.." to list");
+	if name ~= "NOBODY" then
+		local str = name.. " - "..time;
+		flameTable[count] = str;
+		print("tablesize: "..table.getn(flameTable))
+	end
+end
+
+function flameActual() 
+	local message = carrierName.." is holding the Eye for "..countTime().." seconds.";
+	--TODO detect lfr? -.-
+	SendChatMessage(message, "RAID");
+end
+
+function printFlameList() 
+	local j = table.getn(flameTable);
+	local max = 15;
+	if j < max then
+		max = j;
+	end
+	for i=1, max do
+		local message = i.." "..flameTable[j-i+1];
+		--TODO detect lfr? -.-
+		SendChatMessage(message, "RAID");
+	end
+end
+
+
 local updater=CreateFrame("Frame")
 updater:SetScript("OnUpdate", function(self)
 	if (scanEnabled and IsInRaid()) then
 		scanRaidForBuff()
 		if (lastCarrier == carrierName) then
 			--keep time / do nothing
+			lastCarrierTime = countTime();
 		else
+			addToList(tableindex, lastCarrierTime, lastCarrier);
+			tableindex = tableindex+1;
 			lastCarrier = carrierName;
 			holdingSince = GetTime();
 		end
-		sbtext:SetText(carrierName.." "..countTime())
+		if (tonumber(countTime()) < 1 and carrierName == "NOBODY") then
+			sbtext:SetText("");
+		else
+			sbtext:SetText(carrierName.." "..countTime());
+		end
 		sBFrame.text = sbtext;
 	end
 end)
+
+
+
+SLASH_ISKARFLAMEASSIST1 = '/ifa';
+
+
+local function handler(msg, editbox)
+	local command, rest = msg:match("^(%S*)%s*(.-)$");
+	if command == "show" then
+		sBFrame:Show();
+	elseif command == "hide" then
+		sBFrame:Hide();
+	end
+end
+
+SlashCmdList["ISKARFLAMEASSIST"] = handler;
